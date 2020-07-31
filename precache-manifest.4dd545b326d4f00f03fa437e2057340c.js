039dd3a7e701dfeeacfa33c7d52ccb99f490990b
David Perez <dperezval08@gmail.com>
2020-07-30 20:01:46 -0500

    Updates

---
+++ precache-manifest.4dd545b326d4f00f03fa437e2057340c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0610a09ddb82bced9205c7c03fb0095e",
+    "url": "/pokeapp/index.html"
+  },
+  {
+    "revision": "a2871ca31866edd10fe7",
+    "url": "/pokeapp/static/js/2.931cece4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokeapp/static/js/2.931cece4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a95cbc60c2a9eb36cd0",
+    "url": "/pokeapp/static/js/main.6efc2b11.chunk.js"
+  },
+  {
+    "revision": "969db64b723a4e9f3748",
+    "url": "/pokeapp/static/js/runtime-main.3375603f.js"
+  },
+  {
+    "revision": "5d9c284c963c4f40230bf6bdbc96e9de",
+    "url": "/pokeapp/static/media/404.5d9c284c.png"
+  },
+  {
+    "revision": "01f28b7e050e3b4aec78321076ba4663",
+    "url": "/pokeapp/static/media/pokemon-arrow.01f28b7e.png"
+  },
+  {
+    "revision": "8859e86fccefbfddeb4b28a047639ab4",
+    "url": "/pokeapp/static/media/pokemon-logo.8859e86f.png"
+  },
+  {
+    "revision": "f6a3ec2ce40e91ad3a896710916e76e1",
+    "url": "/pokeapp/static/media/simple_pokeball.f6a3ec2c.gif"
+  }
+]);
